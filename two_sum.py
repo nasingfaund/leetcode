64690ddd5fcca7e129d51fddcdd9ac5a43bf1259
funnydman <crazy-dzmitry@mail.ru>
2020-11-23 15:06:56 +0300

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,15 +1,32 @@
-# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# https://leetcode.com/problems/two-sum/
+    
+    
+# Brute force 
+def twoSum(nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
 
-# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# Two-pass Hash Table
+def twoSum(nums: List[int], target: int) -> List[int]:
+    seen = {}
+    for i, v in enumerate(nums):
+        seen[v] = i
 
-# You can return the answer in any order.
+    for i, v in enumerate(nums):
+        rem = target - v
 
+        if rem in seen and seen[rem] != i:
+            return [seen[rem], i]
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+# One-pass Hash Table
+def twoSum(nums: List[int], target: int) -> List[int]:
+    seen = {}
+    for i, v in enumerate(nums):
+        rem = target - v
 
-        
+        if rem in seen:
+            return [seen[rem], i]
+
+        seen[v] = i
