781c2c4ca6c06b430df227d6421a6caaa3c21e1e
funnydman <crazy-dzmitry@mail.ru>
2020-11-23 12:54:20 +0300

    fix naming issue

--- problems/matrix/set_matrix_zeroes.py
+++ problems/matrix/set_matrix_zeroes.py
@@ -3,16 +3,14 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix)
-        m = len(matrix[0])
-        
-        coordinates = [[0 for i in range(n)] for j in range(m)]
+        m = len(matrix)
+        n = len(matrix[0])
 
+        coordinates = [[0 for i in range(n)] for j in range(m)]
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
                     coordinates[i][j] = 1
-
 
         for i in range(m):
             for j in range(n):
@@ -20,6 +18,7 @@ class Solution:
                     for k in range(n):
                         matrix[i][k] = 0
                     # zeroing the column
-                    for n in range(m):
-                        matrix[n][j] = 0
+                    for c in range(m):
+                        matrix[c][j] = 0
+
 
