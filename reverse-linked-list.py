80c76b46aeccb3256905ed4ca0555d07e711b6d1
funnydman <crazy-dzmitry@mail.ru>
2020-11-27 15:54:01 +0300

    Update reverse-linked-list.py

--- reverse-linked-list.py
+++ reverse-linked-list.py
@@ -3,16 +3,26 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        curr = head
-        last = None
-        while curr != None:
-            n = ListNode(curr.val)
-            n.next = last
 
-            last = n
+def reverseList(self, head: ListNode) -> ListNode:
+    curr = head
+    last = None
+    while curr != None:
+        n = ListNode(curr.val)
+        n.next = last
 
-            curr = curr.next
-        return last
+        last = n
 
+        curr = curr.next
+    return last
+
+def reverse_list(head):
+    new_head = None
+    while head != None:
+        tmp = head.next
+        head.next = new_head
+
+        new_head = head
+        head = tmp
+
+    return new_head
