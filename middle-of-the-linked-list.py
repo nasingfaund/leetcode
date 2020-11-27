d31e4a236a0bc5faa8b355afb820b6bdecb74cdd
funnydman <crazy-dzmitry@mail.ru>
2020-11-27 12:42:15 +0300

    Update middle-of-the-linked-list.py

--- middle-of-the-linked-list.py
+++ middle-of-the-linked-list.py
@@ -6,21 +6,30 @@ https://leetcode.com/problems/middle-of-the-linked-list/solution/
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def middleNode(self, head: ListNode) -> ListNode:
-        length = 0
-        curr = head
 
-        while curr != None:
-            length +=1
-            curr = curr.next
 
-        middle = length // 2
+# O(n), O(n)
+def middleNode(head: ListNode) -> ListNode:
+    length = 0
+    curr = head
 
-        c = head
-        for _ in range(middle):
-            c = c.next
+    while curr != None:
+        length +=1
+        curr = curr.next
 
-        return c
+    middle = length // 2
+
+    c = head
+    for _ in range(middle):
+        c = c.next
+
+    return c
 
         
+# approach 2 - O(n), O(1)
+def middle_node(head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    return slow
