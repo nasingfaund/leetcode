9cd8447255357466b03a999ee593dec31b9db084
funnydman <crazydzmitry@gmail.com>
2021-01-03 17:16:55 +0300

    Create 461. Hamming Distance.py

---
+++ 461. Hamming Distance.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        n = x ^ y
+        res = 0
+        while n != 0:
+            res += (1 & n)
+            n >>=1
+        return res
+        
