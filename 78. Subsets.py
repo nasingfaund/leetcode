7f5e75cc3eebf8b6ba18346fd2f201b0cb039553
funnydman <crazydzmitry@gmail.com>
2021-01-27 11:06:56 +0300

    Create 78. Subsets.py

---
+++ 78. Subsets.py
@@ -0,0 +1,31 @@
+# bit manipulation
+class Solution:
+    def subsets(self, nums):
+        res = []
+        for i in range(1 << len(nums)):
+            v = bin(i)[2:]
+            aset = []
+            if len(v) != len(nums):
+                v = v.zfill(len(v) + (len(nums) - len(v)))
+
+            for j, value in enumerate(v):
+                if value == '1':
+                    aset.append(nums[j])
+            if aset not in res:
+                res.append(aset)
+        return res
+
+
+class Solution:
+    def subsets(self, nums):
+        res = []
+        for i in range(1 << len(nums)):
+            aset = []
+            for j in range(len(nums)):
+                value = (1 << j) & i
+                if value:
+                    aset.append(nums[j])
+            res.append(aset)
+        return res
+
+        
