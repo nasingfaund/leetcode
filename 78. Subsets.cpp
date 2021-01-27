73f436f2e39f56d1da4a0e344634d4933d268cf2
funnydman <crazydzmitry@gmail.com>
2021-01-27 11:20:21 +0300

    Update 78. Subsets.cpp

--- 78. Subsets.cpp
+++ 78. Subsets.cpp
@@ -17,3 +17,23 @@ public:
         return res;
     }
 };
+
+
+// improved 
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int value;
+        const int n = nums.size(), p = (1<<n);
+        vector<vector<int>> res(p);
+        for(int i=0; i < p; i++) {
+            for(int j=0; j < n; j++) {
+                value = (i >> j) & 1;
+                if(value) {
+                    res[i].push_back(nums[j]);
+                }
+            }
+        }
+        return res;
+    }
+};
