75e5397e3dd5b914e7d50af67dbb556d44142482
funnydman <crazydzmitry@gmail.com>
2021-07-01 21:10:25 +0300

    Create 73. Set Matrix Zeroes.py

---
+++ 73. Set Matrix Zeroes.py
@@ -0,0 +1,59 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        m = len(matrix[0])
+        col_has_zero = False
+        row_has_zero = False
+        for j in range(m):
+            if matrix[0][j] == 0:
+                row_has_zero = True
+        for j in range(n):
+            if matrix[j][0] == 0:
+                col_has_zero = True
+
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        for i in range(1, m):
+            if matrix[0][i] == 0:
+                for j in range(n):
+                    matrix[j][i] = 0
+
+        for i in range(1, n):
+            if matrix[i][0] == 0:
+                for j in range(m):
+                    matrix[i][j] = 0
+
+        if row_has_zero:
+            for j in range(m):
+                matrix[0][j] = 0
+
+        if col_has_zero:
+            for j in range(n):
+                matrix[j][0] = 0
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        m = len(matrix[0])
+        indexes = []
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    indexes+=[(i, n) for n in range(m)]
+                    indexes+=[(n, j) for n in range(n)]
+
+        for i, j in indexes:
+            matrix[i][j] = 0
+
+        return matrix
