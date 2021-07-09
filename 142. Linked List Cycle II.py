5e1bb2698f7839a66898fd28ec43913d0b7e9bed
funnydman <crazydzmitry@gmail.com>
2021-07-09 17:06:24 +0300

    Update 142. Linked List Cycle II.py

--- 142. Linked List Cycle II.py
+++ 142. Linked List Cycle II.py
@@ -7,3 +7,18 @@ class Solution:
             passed.append(head)
             head = head.next
         
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        fast = slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast: 
+                slow2 = head
+                while slow2!=slow:
+                    slow = slow.next
+                    slow2 = slow2.next
+                return slow 
+        
+        
