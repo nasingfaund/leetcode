5d278bd74942f937d0a98fa8d58b6d2108bc8317
funnydman <crazydzmitry@gmail.com>
2021-07-09 16:19:50 +0300

    Update 206. Reverse Linked List.py

--- 206. Reverse Linked List.py
+++ 206. Reverse Linked List.py
@@ -8,3 +8,13 @@ class Solution:
             head = tmp
             
         return new_head
+    
+    
+class Solution:
+    def reverseList(self, head):
+        if not head or not head.next:
+            return head
+        node = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return node
