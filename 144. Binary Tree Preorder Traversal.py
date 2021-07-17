e09f913eddd92555b605c21b37f24326601ff833
funnydman <crazydzmitry@gmail.com>
2021-07-17 10:28:46 +0300

    Create 144. Binary Tree Preorder Traversal.py

---
+++ 144. Binary Tree Preorder Traversal.py
@@ -0,0 +1,7 @@
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+        
