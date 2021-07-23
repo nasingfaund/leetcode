0989cbb9c06f8c8b8ff63bce24f457be8bb0bcfb
funnydman <crazydzmitry@gmail.com>
2021-07-23 14:06:17 +0300

    Update 512. Game Play Analysis II.py

--- 512. Game Play Analysis II.py
+++ 512. Game Play Analysis II.py
@@ -1,2 +1,6 @@
 # Write your MySQL query statement below
 select player_id, device_id from Activity where (player_id, event_date) in (select player_id, min(event_date) as event_date from Activity group by player_id);
+
+
+select distinct player_id, first_value(device_id) over (partition by player_id order by event_date) as device_id
+from activity;
