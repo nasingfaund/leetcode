2d041dcaafe6c0435c4bcf1700a93779b33e91e8
funnydman <crazydzmitry@gmail.com>
2021-07-23 20:14:04 +0300

    Create 1407. Top Travellers.py

---
+++ 1407. Top Travellers.py
@@ -0,0 +1,4 @@
+select Users.name, coalesce(sum(Rides.distance), 0) as travelled_distance
+from Users
+         left join Rides on Users.id = Rides.user_id group by user_id,name
+order by travelled_distance desc, name ;
