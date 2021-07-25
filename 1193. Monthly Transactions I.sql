270aee590609ba584cae0cf6fcec19934e5d5d3a
funnydman <crazydzmitry@gmail.com>
2021-07-25 13:33:36 +0300

    Create 1193. Monthly Transactions I.sql

---
+++ 1193. Monthly Transactions I.sql
@@ -0,0 +1,1 @@
+select country, substr(trans_date::text, 0, 8) as month, count(state) as trans_count, sum(case when state='approved' then 1 else 0 end) as approved_count,sum(amount) as trans_total_amount, sum(Case when state='approved' then amount else 0 end) as  approved_total_amount from Transactions group by month, country;
