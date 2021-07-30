b7b5943e08a3b2e944e03705b71b41ee142e8376
funnydman <crazydzmitry@gmail.com>
2021-07-30 12:09:35 +0300

    Update 1445. Apples & Oranges.sql

--- 1445. Apples & Oranges.sql
+++ 1445. Apples & Oranges.sql
@@ -2,3 +2,7 @@ select sale_date,
        sum(case when fruit = 'apples' then sold_num else -sold_num end) diff
 from sales
 group by sale_date order by sale_date;
+
+SELECT sale_date,
+SUM(CASE WHEN fruit = 'apples' THEN sold_num END) - SUM(CASE WHEN fruit = 'oranges' THEN sold_num END) as diff
+FROM Sales GROUP BY sale_date ORDER BY sale_date
