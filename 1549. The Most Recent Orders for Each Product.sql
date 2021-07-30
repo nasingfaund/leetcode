28f1011f067a82a450723bd6453e6dd1d6bac965
funnydman <crazydzmitry@gmail.com>
2021-07-30 11:44:02 +0300

    Update 1549. The Most Recent Orders for Each Product.sql

--- 1549. The Most Recent Orders for Each Product.sql
+++ 1549. The Most Recent Orders for Each Product.sql
@@ -1,8 +1,7 @@
 select product_name, Orders.product_id, order_id, order_date
 from Orders
          inner join Products on Orders.product_id = Products.product_id
-         inner join Customers on Orders.customer_id = Customers.customer_id
-where (Orders.product_id, Orders.order_date) in (select Orders.product_id, max(order_date)
-                                                 from orders
-                                                          inner join Products P on Orders.product_id = P.product_id
-                                                 group by orders.product_id) order by product_name,product_id,order_id;
+where (Orders.product_id, Orders.order_date) in (select product_id, max(order_date) as order_date
+                                                 from Orders
+                                                 group by product_id)
+order by product_name, product_id, order_id;
