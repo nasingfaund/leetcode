968aee6933078eacfd0303ef328f2364c902d825
funnydman <crazydzmitry@gmail.com>
2021-08-08 13:51:01 +0300

    Update 1661. Average Time of Process per Machine.sql

--- 1661. Average Time of Process per Machine.sql
+++ 1661. Average Time of Process per Machine.sql
@@ -3,3 +3,9 @@ select machine_id,
         case when activity_type = 'start' then timestamp else 0 end) / count(distinct activity_type))::numeric, 3) as processing_time
 from Activity
 group by machine_id;
+
+
+select machine_id,
+       round((sum(case when activity_type = 'end' then timestamp else -timestamp end) / count(distinct activity_type))::numeric, 3) as processing_time
+from Activity
+group by machine_id;
