28d9a28c3a3671122cffdec328a38335d68d75e5
funnydman <crazydzmitry@gmail.com>
2021-08-08 13:00:20 +0300

    Update 1571. Warehouse Manager.sql

--- 1571. Warehouse Manager.sql
+++ 1571. Warehouse Manager.sql
@@ -1,6 +1,1 @@
-select name as warehouse_name, sum(units*T.volume) as volume
-from (
-         select  name, units, Width * Length * Height as volume
-         from Warehouse W
-                  inner join Products P on W.product_id = P.product_id) T
-group by name;
+select name as warehouse_name, sum(width*length*height*units) as volume from Warehouse W inner join Products P on W.product_id = P.product_id group by name;
