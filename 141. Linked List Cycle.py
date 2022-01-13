de8987446249a75bf91ed5f54077ed94a48b1bf9
funnydman <crazydzmitry@gmail.com>
2022-01-13 12:29:19 +0100

    Update 141. Linked List Cycle.py

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -2,6 +2,20 @@ class Solution:
     def hasCycle(self, head: ListNode) -> bool:
         if not head:
             return False
+        temp = head
+        seen = set()
+        while temp.next:
+            if temp in seen:
+                return True
+            seen.add(temp)
+            temp = temp.next
+        return False
+        
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head:
+            return False
         slow = fast = head
         while fast and fast.next:
             slow = slow.next
