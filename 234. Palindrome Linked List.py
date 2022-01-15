ecdec02ec7c9dfa3e318b07e62dcc7487dc3d776
funnydman <crazydzmitry@gmail.com>
2022-01-15 11:56:58 +0100

    Update 234. Palindrome Linked List.py

--- 234. Palindrome Linked List.py
+++ 234. Palindrome Linked List.py
@@ -1,3 +1,5 @@
+
+# stack
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
         stack = []
@@ -12,5 +14,27 @@ class Solution:
             if val != head.val:
                 return False
             head = head.next
+        return True
+            
+# with revert
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        rev_head  = None
+        temp = head
+        while temp:
+            node = ListNode(temp.val)
+            node.next = rev_head
+            rev_head = node
+            
+            temp = temp.next
+            
+        while head:
+            if head.val!=rev_head.val:
+                return False
+            
+            head = head.next
+            rev_head = rev_head.next
+            
         return True
             
