e8bee571d90d6a6812cd37a681d185523bcdf022
funnydman <crazydzmitry@gmail.com>
2022-01-18 08:52:45 +0100

    Update 160. Intersection of Two Linked Lists.py

--- 160. Intersection of Two Linked Lists.py
+++ 160. Intersection of Two Linked Lists.py
@@ -10,3 +10,17 @@ class Solution(object):
             q = q.next if q else headA 
 
         return p
+
+    
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        s = set()
+        while headA:
+            s.add(headA)
+            headA = headA.next
+
+        while headB:
+            if headB in s:
+                return headB
+            headB = headB.next
+        return None
