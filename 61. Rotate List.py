462bb7b6e8d037008e5ea3ef7ac4025c1b19ecc9
funnydman <crazydzmitry@gmail.com>
2022-01-18 20:10:52 +0100

    Update 61. Rotate List.py

--- 61. Rotate List.py
+++ 61. Rotate List.py
@@ -1,47 +1,44 @@
 class Solution:
-    def revertList(self, head):
-        dummy_head  = None 
-        curr = head
-        while curr:
-            node = ListNode(curr.val)
-            node.next = dummy_head
-            dummy_head = node
-            
-            curr = curr.next
-        return dummy_head
-    
-    def get_length(self, head):
-        temp = head
-        length = 0
-        while temp:
-            temp = temp.next
-            length +=1
-        return length
-        
-    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if not head:
             return head
         
-        reverted_list = temp = self.revertList(head)
-        length = self.get_length(head)
-        
-        k = k % length
-        while k > 0:
-            if not temp:
-                temp = reverted_list
-            node = ListNode(temp.val)
-            node.next = head
-            head = node
-            
+        temp = head
+        length = 1
+        while temp.next:
+            length +=1
             temp = temp.next
-            k-=1
             
-        curr = head
-        while length - 1 > 0 and curr:
-            curr = curr.next
-            length -=1
-            
-        if curr:
-            curr.next = None
+        temp.next = head
         
+        it = k % length 
+        
+        for i in range(length-it):
+            temp = temp.next
+            
+        head = temp.next
+        temp.next = None
         return head
+    
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return head
+        
+        tail = head
+        length = 1
+        while tail.next:
+            length +=1
+            tail = tail.next
+            
+        tail.next = head
+        
+        it = k % length 
+        
+        for i in range(length-it):
+            tail = tail.next
+            
+        head = tail.next
+        tail.next = None
+        return head
+            
