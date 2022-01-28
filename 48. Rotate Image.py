38b3b38177fc81143e2d7338cab0b50e2846150d
funnydman <crazydzmitry@gmail.com>
2022-01-28 18:43:08 +0100

    Create 48. Rotate Image.py

---
+++ 48. Rotate Image.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range((n + 1)//2): # row
+            for j in range(n//2): # columns
+                temp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = temp
