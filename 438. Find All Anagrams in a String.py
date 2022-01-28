ddda5dfb466815fdc4c321701888794cf60ba6b0
funnydman <crazydzmitry@gmail.com>
2022-01-28 13:41:42 +0100

    Update 438. Find All Anagrams in a String.py

--- 438. Find All Anagrams in a String.py
+++ 438. Find All Anagrams in a String.py
@@ -78,3 +78,24 @@ class Solution:
                 output.append(i)
 
         return output
+# Slight variation
+from collections import Counter
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        output = []
+
+        dic_p = Counter(p)
+        dic_s = Counter(s[:len(p) - 1])
+
+        if dic_s == dic_p:
+            output.append(0)
+
+        for i in range(len(p) - 1, len(s)):
+            dic_s[s[i]] += 1
+            if dic_s == dic_p:
+                output.append(i - len(p) + 1)
+            dic_s[s[i - len(p) + 1]] -= 1
+            if dic_s[s[i - len(p) + 1]] == 0:
+                del dic_s[s[i - len(p) + 1]]
+
+        return output
