43438d30c769bdfa8a1ab5e573a9f1eae9157b05
funnydman <crazydzmitry@gmail.com>
2022-01-31 08:53:30 +0100

    Update and rename move-zeroes.py to 283. Move Zeroes.py

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        pos = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos += 1
+        return nums
+
+nums = [0, 1, 0, 3, 12]
+print(move_zeroes(nums))
+# [1, 3, 12, 0, 0]
