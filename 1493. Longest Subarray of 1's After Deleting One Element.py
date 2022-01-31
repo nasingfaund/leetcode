81ad650bb1671fcaf2a072e6305652a131b688a8
funnydman <crazydzmitry@gmail.com>
2022-01-31 10:48:54 +0100

    Update 1493. Longest Subarray of 1's After Deleting One Element.py

--- 1493. Longest Subarray of 1's After Deleting One Element.py
+++ 1493. Longest Subarray of 1's After Deleting One Element.py
@@ -2,6 +2,24 @@ class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         max_len = 0
         start = 0
+        max_ones_count = 0
+        for end in range(len(nums)):
+            if nums[end] == 1:
+                max_ones_count +=1
+            
+            while end - start + 1 - max_ones_count > 1:
+                if nums[start] == 1:
+                    max_ones_count -=1
+                start +=1
+            
+            max_len = max(max_len, end-start)
+
+        return max_len
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        max_len = 0
+        start = 0
         adict = {}
         for end in range(len(nums)):
             if nums[end] not in adict:
