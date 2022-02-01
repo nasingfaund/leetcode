b871dd78f42b5d64bc70987159408af180bb8c48
funnydman <crazydzmitry@gmail.com>
2022-02-01 22:19:22 +0100

    Update 24. Swap Nodes in Pairs.py

--- 24. Swap Nodes in Pairs.py
+++ 24. Swap Nodes in Pairs.py
@@ -13,5 +13,20 @@ class Solution:
         return node
         
         
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
         
+        cur = dummy
         
+        while cur.next and cur.next.next:
+            first = cur.next
+            sec = cur.next.next
+            first.next = sec.next
+            cur.next = sec
+            sec.next = first
+            cur = cur.next.next
+        return dummy.next
