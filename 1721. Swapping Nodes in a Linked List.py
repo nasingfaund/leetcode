b6e77cc118b6a97f59371f7f06b7788312867dee
funnydman <crazydzmitry@gmail.com>
2022-02-02 08:57:36 +0100

    Update 1721. Swapping Nodes in a Linked List.py

--- 1721. Swapping Nodes in a Linked List.py
+++ 1721. Swapping Nodes in a Linked List.py
@@ -18,3 +18,46 @@ class Solution:
         second.val = temp
         
         return head
+
+# swapping node values    
+class Solution:
+    def swapNodes(self, head: ListNode, k: int) -> ListNode:
+        first = last = head
+        for _ in range(k-1):
+            first = first.next
+            
+        node = first
+        while node.next:
+            node = node.next
+            last = last.next
+        
+        temp = first.val
+        first.val = last.val
+        last.val = temp
+        
+        return head
+    
+# two pointers
+class Solution:
+    def swapNodes(self, head: ListNode, k: int) -> ListNode:
+        dummy =  pre_right = pre_left = ListNode(-1)
+        dummy.next = pre_right.next = pre_left.next =  head
+        
+        right = left = head
+        for i in range(k-1):
+            pre_left = left
+            left = left.next
+
+        temp = left
+
+        while temp.next:
+            pre_right = right
+            right = right.next
+            temp = temp.next
+
+        if left == right:
+            return head
+
+        pre_left.next, pre_right.next = right, left
+        left.next, right.next = right.next, left.next
+        return dummy.next 
