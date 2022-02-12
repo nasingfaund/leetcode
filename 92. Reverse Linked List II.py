8158fed7fba2aabf60f211453e70f3f82459ada2
funnydman <crazydzmitry@gmail.com>
2022-02-12 10:28:56 +0100

    Update 92. Reverse Linked List II.py

--- 92. Reverse Linked List II.py
+++ 92. Reverse Linked List II.py
@@ -1,4 +1,32 @@
 class Solution:
+    def reverseBetween(self, head: Optional[ListNode], p: int, q: int) -> Optional[ListNode]:
+        if p == q:
+            return head
+
+        current, previous = head, None
+        for i in range(p-1):
+            previous = current
+            current = current.next
+
+        last_node_of_first_part = previous
+        last_node_of_sub_list = current
+        
+        temp = None  
+        for i in range(q-p+1):
+            temp = current.next
+            current.next = previous
+            previous = current
+            current = temp
+
+        if last_node_of_first_part:
+            last_node_of_first_part.next = previous
+        else:
+            head = previous
+
+        last_node_of_sub_list.next = current
+        return head
+    
+class Solution:
     def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
         counter = 0
         stack = []
