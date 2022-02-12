03fa005b2f73811a18457f6c6c1d7c512e5117ed
funnydman <crazydzmitry@gmail.com>
2022-02-12 11:17:35 +0100

    Update 25. Reverse Nodes in k-Group.py

--- 25. Reverse Nodes in k-Group.py
+++ 25. Reverse Nodes in k-Group.py
@@ -1,3 +1,53 @@
+# after educative https://www.educative.io/courses/grokking-the-coding-interview/RMZylvkGznR
+class Solution:
+    def reverseBetween(self, head: ListNode, p: int, q: int) -> ListNode:
+        if p == q:
+            return head
+
+        current, previous = head, None
+        for i in range(p - 1):
+            previous = current
+            current = current.next
+
+        last_node_of_first_part = previous
+        last_node_of_sub_list = current
+        
+        for i in range(q - p + 1):
+            temp = current.next
+            current.next = previous
+            previous = current
+            current = temp
+
+        if last_node_of_first_part:
+            last_node_of_first_part.next = previous
+        else:
+            head = previous
+
+        last_node_of_sub_list.next = current
+        return head
+
+    def get_length(self, head):
+        temp = head
+        length = 0
+        while temp:
+            length += 1
+            temp = temp.next
+        return length
+
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        p = 1
+        q = k
+        length = self.get_length(head)
+
+        it = length // k
+        for i in range(it):
+            head = self.reverseBetween(head, p, q)
+            p +=k
+            q += k
+
+        return head
+
+
 class Solution:
     def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
         counter = 0
