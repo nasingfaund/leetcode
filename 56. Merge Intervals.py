16f30b866f999fb31215d1c9d819bf6d0b2f5b6a
funnydman <crazydzmitry@gmail.com>
2022-02-16 12:03:58 +0100

    Update 56. Merge Intervals.py

--- 56. Merge Intervals.py
+++ 56. Merge Intervals.py
@@ -13,4 +13,15 @@ class Solution:
         intervals.sort(key=lambda x: (x[0], x[1]))
         return self._merge(intervals)
                                
+# O(nlog⁡n)
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        merged = []
+        intervals.sort(key=lambda x: x[0])
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+        return merged
         
