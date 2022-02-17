790cdd7c35896ef2c84c97c965cef2c6f2efb485
funnydman <crazydzmitry@gmail.com>
2022-02-17 22:13:13 +0100

    Create 435. Non-overlapping Intervals.py

---
+++ 435. Non-overlapping Intervals.py
@@ -0,0 +1,9 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals):
+        end, count = float('-inf'), 0
+        for s, e in sorted(intervals, key=lambda x: x[1]):
+            if s >= end: 
+                end = e
+            else: 
+                count += 1
+        return count
