c94f0900ca5184fa41b95bdc3bd4e8386431c0bd
funnydman <crazydzmitry@gmail.com>
2022-03-08 10:04:45 +0100

    Update 102. Binary Tree Level Order Traversal.py

--- 102. Binary Tree Level Order Traversal.py
+++ 102. Binary Tree Level Order Traversal.py
@@ -23,3 +23,24 @@ class Solution:
             result.append(curr_level)
         return result
         
+
+from collections import deque
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return root
+        result = []
+        queue = deque()
+        queue.append(root)
+        while queue:
+            queue_len = len(queue)
+            curr_level = []
+            for _ in range(queue_len):
+                root = queue.popleft()
+                curr_level.append(root.val)
+                if root.left:
+                    queue.append(root.left)
+                if root.right:
+                    queue.append(root.right)
+            result.append(curr_level)
+        return result
