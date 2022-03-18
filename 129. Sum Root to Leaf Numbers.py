5466903eae0dba8178b5b6601089bc7f132110cd
funnydman <crazydzmitry@gmail.com>
2022-03-18 08:43:02 +0100

    Create 129. Sum Root to Leaf Numbers.py

---
+++ 129. Sum Root to Leaf Numbers.py
@@ -0,0 +1,28 @@
+class Solution:
+    def _sumNumers(self, root, path_sum):
+        if not root:
+            return 0
+        path_sum = path_sum*10 + root.val
+        if not root.left and not root.right:
+            return path_sum
+        return self._sumNumers(root.left, path_sum) + self._sumNumers(root.right, path_sum)
+    
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        return self._sumNumers(root, 0)
+
+      
+ # bfs     
+ class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        queue, total_sum = deque([(root, 0)]), 0
+        while queue:
+            root, cur = queue.pop()
+            cur = cur * 10 + root.val
+            if not root.left and not root.right: 
+                total_sum += cur
+            if root.right: 
+                queue.append((root.right, cur))
+            if root.left: 
+                queue.append((root.left, cur))
+        return total_sum
+        
