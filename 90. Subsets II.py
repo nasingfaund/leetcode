fd2082360b600d0df8dc4089ed86f77d767eb53e
funnydman <crazydzmitry@gmail.com>
2022-05-11 18:10:22 +0200

    Update 90. Subsets II.py

--- 90. Subsets II.py
+++ 90. Subsets II.py
@@ -9,3 +9,23 @@ class Solution:
                 if subset not in subsets:
                     subsets.append(subset)
         return subsets
+
+    
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = [[]]
+        start_index, end_index = 0, 0
+        for i in range(len(nums)):
+            start_index = 0
+            if i >= 1 and nums[i] == nums[i-1]:
+                start_index = end_index
+                
+            end_index = len(result)
+            for j in range(start_index, end_index):
+                sub = list(result[j])
+                sub.append(nums[i])
+                result.append(sub)
+        return result
+                
+            
