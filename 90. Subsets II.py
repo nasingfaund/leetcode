b6395f7d5bfe59b6c3236d88e136b55f0650408f
funnydman <crazydzmitry@gmail.com>
2022-05-18 16:54:46 +0200

    Update 90. Subsets II.py

--- 90. Subsets II.py
+++ 90. Subsets II.py
@@ -1,5 +1,20 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        self.dfs(sorted(nums), [], result)
+        return result 
+    
+    def dfs(self, nums, path, result):
+        result.append(path)
+        for i in range(len(nums)):
+            if i> 0 and nums[i] == nums[i-1]:
+                continue
+            self.dfs(nums[i+1:], path + [nums[i]], result)
+        
+
+# first version
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         subsets = [[]]
         nums.sort()
         for num in nums:
