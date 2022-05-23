467ea9f731fb0aa5db664bbe7ea0d228580ef4d6
funnydman <crazydzmitry@gmail.com>
2022-05-23 14:54:28 +0200

    Update 62. Unique Paths.py

--- 62. Unique Paths.py
+++ 62. Unique Paths.py
@@ -1,16 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        matrix = [[0 for i in range(n)] for j in range(m)]
-        
-        for i in range(m):
-            matrix[i][0] = 1
-            
-        for j in range(n):
-            matrix[0][j] = 1
+        matrix = [[1 for i in range(n)] for j in range(m)]
             
         for i in range(1, m):
             for j in range(1, n):
                 matrix[i][j] = matrix[i-1][j] + matrix[i][j-1]
                 
         return matrix[m-1][n-1]
-        
