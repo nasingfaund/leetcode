cc5786c1cda9617cbf0510639bae7f3475f6e8b5
funnydman <crazydzmitry@gmail.com>
2022-05-27 17:29:41 +0200

    Update 518. Coin Change 2.py

--- 518. Coin Change 2.py
+++ 518. Coin Change 2.py
@@ -15,3 +15,29 @@ class Solution:
                     dp[i][j] = dp[i-1][j]+dp[i][j-coins[i-1]]
                         
         return dp[n][amount]
+    
+class Solution:
+    def change(self, amount: int, coins) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for i in range(len(coins)):
+            for j in range(1, amount + 1):
+                if (j - coins[i]) >= 0:
+                    dp[j] += dp[j - coins[i]]
+        return dp[amount]
+
+class Solution:
+    def change(self, amount: int, coins) -> int:
+        @lru_cache(maxsize=None)
+        def dfs(amount, coins, idx, curr):
+            if curr > amount or idx >= len(coins):
+                return 0
+            if amount == curr:
+                return 1
+            result = 0
+
+            result += dfs(amount, coins, idx, curr + coins[idx])
+            result += dfs(amount, coins, idx + 1, curr)
+            return result
+
+        return dfs(amount, tuple(coins), 0, 0)
