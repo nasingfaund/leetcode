d93a2e2772ff456e0d031eaf5ae26b32248f1684
funnydman <crazydzmitry@gmail.com>
2022-05-29 16:17:38 +0200

    Update 78. Subsets.py

--- 78. Subsets.py
+++ 78. Subsets.py
@@ -29,3 +29,12 @@ class Solution:
         return res
 
         
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        for i in range(len(nums)):
+            for j in range(len(result)):
+                subset = list(result[j])
+                subset.append(nums[i])
+                result.append(subset)
+        return result
