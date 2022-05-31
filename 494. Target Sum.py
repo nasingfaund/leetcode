b4a1ea6a61d883b26663f93b5c9ce565163b5343
funnydman <crazydzmitry@gmail.com>
2022-05-31 18:25:51 +0200

    Update 494. Target Sum.py

--- 494. Target Sum.py
+++ 494. Target Sum.py
@@ -53,4 +53,16 @@ class Solution:
             return memo[(sum, index)]
         
         return _findTargetSumWays(0, 0)
+    
+class Solution:
+    def findTargetSumWays(self, nums, target):
+        count = defaultdict(int)
+        count[0] = 1
+        for x in nums:
+            step = defaultdict(int)
+            for y in count:
+                step[y + x] += count[y]
+                step[y - x] += count[y]
+            count = step
+        return count[target]
 
