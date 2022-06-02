40aad8e9423a08edb04774ae04286f66b7d6c166
funnydman <crazydzmitry@gmail.com>
2022-06-02 18:20:54 +0200

    Update 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -7,3 +7,12 @@ class Solution:
             curr_sum = max(nums[i], curr_sum + nums[i])
             best = max(best, curr_sum)
         return best
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp = [0]*len(nums)
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(nums[i], nums[i] + dp[i-1])
+        return max(dp)
+            
