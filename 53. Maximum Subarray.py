6b5d7210c85897f7cb98e4f4a4523bba8a417b57
funnydman <crazydzmitry@gmail.com>
2022-06-03 14:55:31 +0200

    Update 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -8,6 +8,17 @@ class Solution:
             best = max(best, curr_sum)
         return best
 
+# Recursive, Time Complexity: O(n), Time Complexity: O(1)
+class Solution:
+    def maxSubArray(self, nums) -> int:
+        def find(index, curr_max):
+            if index >= len(nums):
+                return float('-inf')
+            curr_max = max(nums[index], curr_max + nums[index])
+            return max(curr_max,  find(index + 1, curr_max))
+        return find(0, 0)
+
+    
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         dp = [0]*len(nums)
@@ -15,4 +26,16 @@ class Solution:
         for i in range(1, len(nums)):
             dp[i] = max(nums[i], nums[i] + dp[i-1])
         return max(dp)
+            
+# Memory Limit Exceeded
+class Solution:
+    def maxSubArray(self, nums) -> int:
+        def find(index, curr_max):
+            if index >= len(nums):
+                return []
+            curr_max = max(nums[index], curr_max + nums[index])
+            return [curr_max] + find(index + 1, curr_max)
+
+        return max(find(0, 0))
+
             
