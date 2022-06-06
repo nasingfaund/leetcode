7a6bf3b7a6d56a091bebbd398b92b41be549e774
funnydman <crazydzmitry@gmail.com>
2022-06-06 18:40:14 +0200

    Update and rename 704. Binary Search,py to 704. Binary Search.py

--- 704. Binary Search.py
+++ 704. Binary Search.py
@@ -14,3 +14,22 @@ class Solution:
                 return _search(nums[:middle], target, low, middle-1)
 
         return _search(nums, target, 0, len(nums) - 1)
+    
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            middle = (left + right) // 2
+            print(middle)
+            
+            if target == nums[middle]:
+                return middle
+            
+            if target > nums[middle]:
+                left = middle + 1
+            else:
+                right= middle - 1
+                
+        return -1
+            
