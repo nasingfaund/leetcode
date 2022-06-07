207d1f7f7255794e2046e51f41c7fa29aa78618b
funnydman <crazydzmitry@gmail.com>
2022-06-07 18:24:50 +0200

    Update 542. 01 Matrix.py

--- 542. 01 Matrix.py
+++ 542. 01 Matrix.py
@@ -33,3 +33,27 @@ class Solution:
         return dist
             
             
+            
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        n = len(mat)
+        m = len(mat[0])
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+        queue = deque()
+        for r in range(n):
+            for c in range(m):
+                if mat[r][c] == 0:
+                    queue.append((r, c))
+                else:
+                    mat[r][c] = -1  # Marked as not processed yet!
+
+        while queue:
+            i, j = queue.popleft()
+            for x, y in directions:
+                nr = i + x
+                nc = j + y
+                if 0 <= nr < n and 0 <= nc < m and mat[nr][nc] == -1:
+                    mat[nr][nc] = mat[i][j] + 1
+                    queue.append((nr, nc))
+        return mat
