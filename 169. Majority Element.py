bcb3ecfd8acb14d5a0526ea2b12a9998b6d9a0c1
funnydman <crazydzmitry@gmail.com>
2022-06-07 10:52:10 +0200

    Update 169. Majority Element.py

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -1,6 +1,12 @@
+# time complexity: O(n), space complexity: O(n)
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         adict = Counter()
         for num in nums:
             adict[num] += 1
         return adict.most_common()[0][0]
+    
+class Solution:
+    def majorityElement(self, nums):
+        counts = collections.Counter(nums)
+        return max(counts.keys(), key=counts.get)
