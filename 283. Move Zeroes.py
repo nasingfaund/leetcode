6582f3751a8100873527d8e94f0992c59f0613e0
funnydman <crazydzmitry@gmail.com>
2022-06-07 10:10:13 +0200

    Update 283. Move Zeroes.py

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -1,3 +1,20 @@
+# TLE
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        left = len(nums) - 1
+        while i < len(nums):
+            if nums[i] == 0 and i < left:
+                left -= 1
+                j = i
+                while j < len(nums) - 1:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                    j += 1
+            else:
+                i += 1
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         pos = 0
@@ -6,7 +23,3 @@ class Solution:
                 nums[pos], nums[i] = nums[i], nums[pos]
                 pos += 1
         return nums
-
-nums = [0, 1, 0, 3, 12]
-print(move_zeroes(nums))
-# [1, 3, 12, 0, 0]
