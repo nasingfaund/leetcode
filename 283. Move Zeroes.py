429377d3f8828e71abad6848dcc42b52abec1f96
funnydman <crazydzmitry@gmail.com>
2022-06-08 16:25:12 +0200

    Update 283. Move Zeroes.py

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -1,20 +1,16 @@
-# TLE
+# Time Complexity: O(n), Space Complexity: O(1)
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        i = 0
-        left = len(nums) - 1
-        while i < len(nums):
-            if nums[i] == 0 and i < left:
-                left -= 1
-                j = i
-                while j < len(nums) - 1:
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
-                    j += 1
-            else:
-                i += 1
+    def moveZeroes(self, nums: list) -> None:
+        left = right = 0
+        while right < len(nums):
+            if nums[right] != 0 and nums[left] == 0:
+                nums[left], nums[right] = nums[right], nums[left]
+
+            if nums[left] != 0:
+                left += 1
+                
+            right += 1
+
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         pos = 0
