576b809b8db7b014a20ee005f8d9785f2249520e
funnydman <crazydzmitry@gmail.com>
2022-06-09 16:14:38 +0200

    Update 994. Rotting Oranges.py

--- 994. Rotting Oranges.py
+++ 994. Rotting Oranges.py
@@ -1,3 +1,4 @@
+# time complexity 0(N*M), Space (N*M)
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m = len(grid) # rows
@@ -6,25 +7,25 @@ class Solution:
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         queue = deque()
         level = 0
-        zeroes = 0
-        ones = 0
+        empty_cells = 0
+        freshes = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
                     queue.append((i, j))
                 elif grid[i][j] == 0:
-                    zeroes +=1
+                    empty_cells +=1
                 elif grid[i][j] == 1:
-                    ones += 1
+                    freshes += 1
         if not queue:
-            if zeroes and not ones:
+            if empty_cells and not freshes:
                 return 0
-            if ones and not zeroes:
+            if freshes and not empty_cells:
                 return -1
             return -1
                     
-        while queue:
-            for _ in range(len(queue)):
+        while queue: 
+            for _ in range(len(queue)): # 2
                 
                 i, j = queue.popleft()
 
@@ -42,4 +43,42 @@ class Solution:
                 if grid[i][j] == 1:
                     return -1
                 
-        return level - 1
+        return level - 1 # because one time in the end when we do not have anythin to add, levels still be incremented by one.
+
+    
+# impoved (checked other solutions)
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m = len(grid) # rows
+        n = len(grid[0]) # cols
+        
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        queue = deque()
+        level = 0
+        empty_cells = 0
+        freshes = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+                elif grid[i][j] == 0:
+                    empty_cells +=1
+                elif grid[i][j] == 1:
+                    freshes += 1
+
+        while queue: 
+            for _ in range(len(queue)): # 2
+                
+                i, j = queue.popleft()
+
+                for x, y in directions:
+                    new_i = i + x
+                    new_j = j + y
+
+                    if 0<= new_i < m and 0<= new_j < n and grid[new_i][new_j] == 1:
+                        freshes -= 1
+                        grid[new_i][new_j] = 2
+                        queue.append((new_i, new_j))
+            level += 1
+        
+        return -1 if freshes else max(level-1, 0)
