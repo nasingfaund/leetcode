2d70d64456e728dbafcda2f729f89aa4bf272a5d
funnydman <crazydzmitry@gmail.com>
2022-06-11 23:19:59 +0200

    Update 136. Single Number.py

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -1,15 +1,16 @@
 """
 Other solutions - https://leetcode.com/problems/single-number/discuss/43000/Python-different-solutions.
 """
-
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(operator.xor, nums)
+        
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         res= 0
         for i in nums:
             res ^=i
         return res
-        
-        
         
         
 #one liner
