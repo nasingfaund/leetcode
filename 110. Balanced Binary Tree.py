b7b9ef4362a8de84cf8f2c242f4ad074b329051f
funnydman <crazydzmitry@gmail.com>
2022-06-11 17:17:12 +0200

    Create 110. Balanced Binary Tree.py

---
+++ 110. Balanced Binary Tree.py
@@ -0,0 +1,17 @@
+class Solution:
+    def dfs(self, root):
+        if not root:
+            return 0
+        
+        left = self.dfs(root.left)
+        right = self.dfs(root.right)
+        
+        if left == -1 or right == -1 or abs(left - right) > 1:
+            return -1
+        return 1 + max(left, right)
+    
+        
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        res = self.dfs(root)
+        return res != -1
+        
