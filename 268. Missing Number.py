ee224020a11e16d041323f18c681d0b7d30b4131
funnydman <crazydzmitry@gmail.com>
2022-06-11 23:00:04 +0200

    Update 268. Missing Number.py

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -1,7 +1,22 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        full_sum = (n+1)*n//2
+        return full_sum - sum(nums)
+       
+ class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        result = len(nums)
+        for i in range(len(nums)):
+            result ^= i
+            result ^= nums[i]
+        return result
+        
+
 # elegant Python solution https://leetcode.com/problems/missing-number/discuss/69832/1%2B-lines-Ruby-Python-Java-C%2B%2B
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        return reduce(operator.xor, nums + list(range(len(nums)+1)))
+        return reduce(operator.xor, nums + range(len(nums)+1))
         
 
 # todo: why this works?
