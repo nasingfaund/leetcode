0649a37463da9d73c161b29914d394cc5608e4be
funnydman <crazydzmitry@gmail.com>
2022-06-12 15:53:39 +0200

    Update 559. Maximum Depth of N-ary Tree.py

--- 559. Maximum Depth of N-ary Tree.py
+++ 559. Maximum Depth of N-ary Tree.py
@@ -1,3 +1,54 @@
+# true bfs
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        queue = deque([root])
+        max_depth = 0
+        while queue:
+            max_depth += 1
+            for _ in range(len(queue)):
+                root = queue.popleft()
+                for node in root.children:
+                    queue.append(node)
+        return max_depth
+    
+# bfs 
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        queue = deque([(root, 1)])
+        visited = set()
+        max_depth = 0
+        while queue:
+            root, depth = queue.popleft()
+            visited.add(root)
+            max_depth = max(max_depth, depth)
+            
+            for node in root.children:
+                if node not in visited:
+                    queue.append((node, depth + 1))
+        return max_depth
+    
+# dfs itertive
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        stack = [(root, 1)]
+        visited = set()
+        max_depth = 0
+        while stack:
+            root, depth = stack.pop()
+            visited.add(root)
+            max_depth = max(max_depth, depth)
+            
+            for node in root.children:
+                if node not in visited:
+                    stack.append((node, depth + 1))
+        return max_depth
+    
 class Solution:
     def dfs(self, root, visited):
         visited.add(root)
