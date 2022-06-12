c4b43bccad3d87cf02ddad13a69ff0e067dbd93b
funnydman <crazydzmitry@gmail.com>
2022-06-12 12:26:24 +0200

    Update 338. Counting Bits.py

--- 338. Counting Bits.py
+++ 338. Counting Bits.py
@@ -5,3 +5,21 @@ class Solution:
             result = [1..5]
             counter.append(counter[i >> 1] + i % 2)
         return counter
+
+class Solution:
+    def countBits(self, num):
+        cache = {}
+
+        def f(num: int) -> int:
+            if num in cache:
+                return cache[num]
+            if num == 0:
+                return 0
+            cache[num] = f(num >> 1) + num % 2
+            return cache[num]
+
+        result = []
+        for i in range(0, num + 1):
+            result.append(f(i))
+        return result
+
