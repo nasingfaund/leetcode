14f8fd93bfcbe8096bc3e0014837b57e3c6ff006
funnydman <crazydzmitry@gmail.com>
2022-06-13 22:03:10 +0200

    Update 110. Balanced Binary Tree.py

--- 110. Balanced Binary Tree.py
+++ 110. Balanced Binary Tree.py
@@ -17,3 +17,31 @@ class Solution:
         
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         return self.dfs(root) != -1
+
+# iterative, based on post order traversal
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        # how about storing parent node? Does not work
+        stack = []
+        last_visited = None
+        adict = defaultdict(int)
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                peek_node = stack[-1]
+                if peek_node.right and last_visited != peek_node.right:
+                    root  = peek_node.right
+                else:
+                    print(peek_node.val)
+                    l = adict[peek_node.left]
+                    r = adict[peek_node.right]
+                    if abs(l - r) > 1:
+                        return False
+                    adict[peek_node] =  max(l, r) + 1 
+                    
+                    last_visited =  stack.pop()
+        return True
