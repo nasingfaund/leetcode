1e6e92cae1371bce83be8d4b83d79b3a79029fb2
funnydman <crazydzmitry@gmail.com>
2022-06-14 17:49:57 +0200

    Create 437. Path Sum III.py

---
+++ 437. Path Sum III.py
@@ -0,0 +1,12 @@
+# bruteforce  N^2
+class Solution(object):
+    def find_paths(self, root, target):
+        if root:
+            return int(root.val == target) + self.find_paths(root.left, target-root.val) + self.find_paths(root.right, target-root.val)
+        return 0
+
+    def pathSum(self, root, sum):
+
+        if root:
+            return self.find_paths(root, sum) + self.pathSum(root.left, sum) + self.pathSum(root.right, sum)
+        return 0
