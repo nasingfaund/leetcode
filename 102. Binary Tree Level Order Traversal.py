70189d8fa1310f9eee657ce2e7138ca345efa5db
funnydman <crazydzmitry@gmail.com>
2022-06-15 16:33:28 +0200

    Update 102. Binary Tree Level Order Traversal.py

--- 102. Binary Tree Level Order Traversal.py
+++ 102. Binary Tree Level Order Traversal.py
@@ -1,41 +1,28 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    def dfs(self, root, adict, level):
         if not root:
-            return root
-        result = []
-        queue = [root]
-        while queue:
-            queue_len = len(queue)
-            curr_level = []
-            for _ in range(queue_len):
-                root = queue.pop(0)
-                curr_level.append(root.val)
-                if root.left:
-                    queue.append(root.left)
-                if root.right:
-                    queue.append(root.right)
-            result.append(curr_level)
-        return result
+            return 
+        adict[level].append(root.val)
         
-
+        self.dfs(root.left, adict, level+1)
+        self.dfs(root.right, adict, level + 1)
+        
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        adict = defaultdict(list)
+        self.dfs(root, adict, 0)
+        return adict.values()
+   
 from collections import deque
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return root
         result = []
-        queue = deque()
-        queue.append(root)
+        queue = deque([root])
+        
         while queue:
-            queue_len = len(queue)
             curr_level = []
-            for _ in range(queue_len):
+            for _ in range(len(queue)):
                 root = queue.popleft()
                 curr_level.append(root.val)
                 if root.left:
