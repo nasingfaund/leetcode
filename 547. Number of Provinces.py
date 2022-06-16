42b959a33e3482affa2bfd2648075fd478354caa
funnydman <crazydzmitry@gmail.com>
2022-06-16 16:42:08 +0200

    Update 547. Number of Provinces.py

--- 547. Number of Provinces.py
+++ 547. Number of Provinces.py
@@ -27,3 +27,21 @@ class Solution:
         return count
 
                 
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        seen = set()
+
+        def dfs(i):
+            for j in range(n):
+                if isConnected[i][j] == 1 and j not in seen:
+                    seen.add(j)
+                    dfs(j)
+
+        count = 0
+        for i in range(n):
+            if i not in seen: 
+                dfs(i)
+                count += 1
+
+        return count
