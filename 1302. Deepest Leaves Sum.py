4d7132cea0a5bac02d51f14ee895ba5e3ca1487d
funnydman <crazydzmitry@gmail.com>
2022-06-18 23:11:43 +0200

    Update 1302. Deepest Leaves Sum.py

--- 1302. Deepest Leaves Sum.py
+++ 1302. Deepest Leaves Sum.py
@@ -10,4 +10,22 @@ class Solution:
             dfs(root.left, depth + 1)
             dfs(root.right, depth + 1)
         dfs(root, 0)
-        return sum(adict.get(max(adict.keys())))    
+        return sum(adict[max(adict.keys())])   
+    
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        self.deepest = 0
+        def dfs(root, depth):
+            if not root:
+                return 0
+            if not root.left and not root.right:
+                if depth == self.deepest:
+                    self.res += root.val
+                elif depth > self.deepest:
+                    self.res = root.val 
+                    self.deepest = depth
+            left = dfs(root.left, depth + 1)
+            right = dfs(root.right, depth + 1)
+            return self.res
+        return dfs(root, 0)
