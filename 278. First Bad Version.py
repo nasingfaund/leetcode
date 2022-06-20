edf052c8ab742b82c17de177c5e518cca2d0de51
funnydman <crazydzmitry@gmail.com>
2022-06-20 15:02:20 +0200

    Update 278. First Bad Version.py

--- 278. First Bad Version.py
+++ 278. First Bad Version.py
@@ -14,3 +14,22 @@ class Solution:
                 first = middle + 1
                 if isBadVersion(first):
                     return first
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        def find(first, second):
+            if first >= second:
+                return first
+            middle = (first + second) // 2
+            
+            if not isBadVersion(middle):
+                if isBadVersion(middle + 1):
+                    return middle  + 1
+            else:
+                if not isBadVersion(middle - 1):
+                    return middle 
+            
+            if isBadVersion(middle):
+                return find(first, middle - 1)
+            else:
+                return find(middle + 1, second)
+        return find(1, n)
