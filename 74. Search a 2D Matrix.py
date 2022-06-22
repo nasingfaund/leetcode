5976d5774f37b7ec78f3c35c89c9063459af5b99
funnydman <crazydzmitry@gmail.com>
2022-06-22 22:22:01 +0200

    Update and rename 74. Search a 2D Matrix to 74. Search a 2D Matrix.py

--- 74. Search a 2D Matrix.py
+++ 74. Search a 2D Matrix.py
@@ -2,6 +2,26 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m  = len(matrix) # rows
         n = len(matrix[0]) # cols
+        
+        first = 0
+        second = n * m - 1
+        
+        while first <= second:
+            middle = first + (second  - first)//2
+            
+            if matrix[middle//n][middle%n] == target:
+                return True
+            if target > matrix[middle//n][middle % n]:
+                first += 1
+            else:
+                second -=1
+        return False
+        
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m  = len(matrix) # rows
+        n = len(matrix[0]) # cols
         row = None
         for i in range(m):
             if matrix[i][0]<= target <= matrix[i][-1]:
