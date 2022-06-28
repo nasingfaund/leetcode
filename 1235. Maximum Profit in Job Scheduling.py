361ade88b8a45289794dfea1bd29335e68aa079c
funnydman <crazydzmitry@gmail.com>
2022-06-28 19:08:16 +0200

    Update 1235. Maximum Profit in Job Scheduling.py

--- 1235. Maximum Profit in Job Scheduling.py
+++ 1235. Maximum Profit in Job Scheduling.py
@@ -1,4 +1,31 @@
-# DP: Time complexity O(N^2)
+# nlogn
+class Solution:
+    def jobScheduling(self, startTime, endTime, profit):
+        intervals = sorted(zip(startTime, endTime, profit), key=lambda x: x[1]) # important: sort by end_time!
+        dp = [[0, 0]]
+        
+        for start_time, end_time, profit in intervals:
+            idx = self.bisect_left(dp, start_time)
+            current_profit = profit+dp[idx][1]
+            if current_profit > dp[-1][1]:
+                dp.append([end_time, current_profit])
+        return dp[-1][1]
+        
+    def bisect_left(self, arr, target):
+        # O(logn)
+        # use binary search to find largest index of 1st tuple element smaller than target 
+        start, end = 0, len(arr)
+        while start < end:
+            mid = (start+end)//2
+            
+            if target < arr[mid][0]:
+                end = mid
+            else:
+                start = mid+1
+                
+        return start-1
+    
+# DP: Time complexity O(N^2) - TLE
 class Solution:
     def jobScheduling(self, startTime, endTime, profit):
         alist = sorted(zip(startTime, endTime, profit), key=lambda x: x[0])
