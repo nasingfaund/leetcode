293a16471e9ecf697f4f72e1521a9c64a2ff0c44
funnydman <crazydzmitry@gmail.com>
2022-06-28 15:37:38 +0200

    Update 300. Longest Increasing Subsequence.py

--- 300. Longest Increasing Subsequence.py
+++ 300. Longest Increasing Subsequence.py
@@ -8,3 +8,19 @@ class Solution:
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j] + 1)
         return max(dp)
+
+    
+# Time Complexity: O(2^n) - TLE
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        def solve(i, prev):
+            if i >= len(nums):
+                return 0
+            take = 0
+            dontTake = solve(i+1, prev)
+            if nums[i] > prev:
+                take = 1 + solve(i +1, nums[i])
+
+            return max(take, dontTake)
+
+        return solve(0, float('-inf'))
