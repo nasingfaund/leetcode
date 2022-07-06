5269ec878e0558f4b991c7f1a15242b85f83d8a0
funnydman <crazydzmitry@gmail.com>
2022-07-06 14:00:10 +0200

    Update 199. Binary Tree Right Side View.py

--- 199. Binary Tree Right Side View.py
+++ 199. Binary Tree Right Side View.py
@@ -1,11 +1,10 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
         result = []
+        if not root:
+            return result
         
-        queue = deque()
-        queue.append(root)
+        queue = deque([root])
         while queue:
             result.append(queue[0].val)
             for _ in range(len(queue)):
@@ -15,4 +14,22 @@ class Solution:
                 if curr.left:
                     queue.append(curr.left)
 
+        return result
+    
+    
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        
+        def dfs(root, depth):
+            if not root:
+                return 
+            
+            if depth == len(result):
+                result.append(root.val)
+                
+            dfs(root.right, depth + 1)  # Go right side first
+            dfs(root.left, depth + 1)
+            
+        dfs(root, 0)
         return result
