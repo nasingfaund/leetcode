2f9b27f6cfd07e77cb3e83eded40a50035ff3b5d
funnydman <crazydzmitry@gmail.com>
2022-07-06 18:13:34 +0200

    Update 98. Validate Binary Search Tree.py

--- 98. Validate Binary Search Tree.py
+++ 98. Validate Binary Search Tree.py
@@ -13,3 +13,14 @@ class Solution:
             return []
         
         return self.inOrder(root.left) + [root.val] + self.inOrder(root.right)
+    
+class Solution:
+    def isValidBST(self, root):
+        def dfs(node, low, high):
+            if not node: 
+                return True
+            if not low < node.val < high: 
+                return False
+            return dfs(node.left, low, node.val) and dfs(node.right, node.val, high)
+        
+        return dfs(root, -float("inf"), float("inf"))
