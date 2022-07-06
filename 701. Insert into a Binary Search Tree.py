33e4e423acdbd284754341be29626c938dea48b0
funnydman <crazydzmitry@gmail.com>
2022-07-06 17:13:01 +0200

    Update 701. Insert into a Binary Search Tree.py

--- 701. Insert into a Binary Search Tree.py
+++ 701. Insert into a Binary Search Tree.py
@@ -1,44 +1,29 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
-# Iterative
 class Solution:
-    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
-        head = root
-        if root is None:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
             return TreeNode(val)
-
-        while root:
-            if val > head.val:
-                if head.right is None:
-                    head.right = TreeNode(val)
-                    return root
-                else:
-                    head = head.right
-            elif val < head.val:
-                if head.left is None:
-                    head.left = TreeNode(val)
-                    return root
-                else:
-                    head = head.left
-        return root
-
-        # Definition for a binary tree node.
-
-# Recursive
-class Solution:
-    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
-        if root is None:
-            return TreeNode(val)
-        
-        if val < root.val:
-            root.left = self.insertIntoBST(root.left, val)
-        elif val > root.val:
+        if val > root.val:
             root.right = self.insertIntoBST(root.right, val)
-        
+        else:
+            root.left = self.insertIntoBST(root.left, val)
+
         return root
-        
+    
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        temp = root
+        while temp:
+            if val > temp.val:
+                if not temp.right:
+                    temp.right = TreeNode(val)
+                    return root
+                else:
+                    temp = temp.right
+            else:
+                if not temp.left:
+                    temp.left = TreeNode(val)
+                    return root
+                else:
+                    temp = temp.left
