70226a6f651d79f8d3ab87520e03cdabd13775ae
funnydman <crazydzmitry@gmail.com>
2022-07-13 21:32:38 +0200

    Update 662. Maximum Width of Binary Tree.py

--- 662. Maximum Width of Binary Tree.py
+++ 662. Maximum Width of Binary Tree.py
@@ -2,13 +2,32 @@ class Solution:
     def widthOfBinaryTree(self, root: TreeNode) -> int:
         res = defaultdict(list)
 
-        def f(level, p, root):
+        def dfs(level, p, root):
             if not root:
                 return p
-            
+
             res[level].append(p)
-            left = f(level + 1, 2 * p, root.left)
-            right = f(level + 1, 2 * p + 1, root.right)
-        
-        f(0, 0, root)
+            dfs(level + 1, 2 * p, root.left)
+            dfs(level + 1, 2 * p + 1, root.right)
+
+        dfs(0, 0, root)
         return max(max(a) - min(a) + 1 for a in res.values())
+
+    
+ class Solution:
+    def widthOfBinaryTree(self, root: TreeNode) -> int:
+
+        queue = deque([[root, 1]])
+        res = 0
+        
+        while queue:
+            nodes = []
+            for _ in range(len(queue)):
+                node, num = queue.popleft()
+                nodes.append(num)
+                if node.left:
+                    queue.append([node.left, 2 * num])
+                if node.right:
+                    queue.append([node.right, 2 * num + 1])
+            res = max(nodes[-1] - nodes[0] + 1, res)
+        return res
