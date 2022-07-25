af69742cf8d913a49f553e2efe41d055698e1e2f
funnydman <crazydzmitry@gmail.com>
2022-07-25 17:40:00 +0200

    Update 22. Generate Parentheses.py

--- 22. Generate Parentheses.py
+++ 22. Generate Parentheses.py
@@ -40,6 +40,46 @@ class Solution:
         generate('', 0, 0, n)
         return result     
         
-
+# backtracking
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        def backtrack(left, right, stack):
+            if left == right == n:
+                result.append("".join(stack))
+                return
+            
+            if left < n:
+                stack.append('(')
+                backtrack(left + 1, right, stack)
+                stack.pop()
+                
+            if right < left:
+                stack.append(')')
+                backtrack(left, right + 1, stack)
+                stack.pop()
+        backtrack(0, 0, [])
+        return res
+   
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        if not n:
+            return []
+        left, right, ans = n, n, []
         
+    
+        def dfs(left, right, ans, string):
+            if right < left:
+                return
+
+            if not left and not right:
+                ans.append(string)
+
+            if left:
+                dfs(left -1, right, ans, string +"(")
+            if right:
+                dfs(left, right-1, ans, string + ")")
+        
+        dfs(left, right, ans, "")
+        return ans
         
