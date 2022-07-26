2bd16b56a05d1f1456d81aa5991a9a2fbfd72355
funnydman <crazydzmitry@gmail.com>
2022-07-26 18:16:34 +0200

    Update 1161. Maximum Level Sum of a Binary Tree.py

--- 1161. Maximum Level Sum of a Binary Tree.py
+++ 1161. Maximum Level Sum of a Binary Tree.py
@@ -11,3 +11,19 @@ class Solution:
             dfs(root.right, level + 1)
         dfs(root, 0)
         return max(result, key=result.get)
+    
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        result = defaultdict(int)
+        
+        stack = [[root, 1]]
+
+        while stack:
+            node, level = stack.pop()
+            result[level] += node.val
+            if node.left:
+                stack.append([node.left, level + 1])
+            if node.right:
+                stack.append([node.right, level + 1])
+        print(result)
+        return max(result, key=result.get)
