8e5d8525eacc76014afc157f74b6c68a882ddb5c
funnydman <crazydzmitry@gmail.com>
2022-07-26 15:02:24 +0200

    Update 88. Merge Sorted Array.py

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -10,3 +10,11 @@ class Solution:
         a = list(nums1[:m])
         b = list(nums2[:n])
         nums1[:] = combine(a, b)[:] 
+    
+    
+from heapq import merge
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        a = list(nums1[:m])
+        b = list(nums2[:n])
+        nums1[:] = list(merge(a, b))[:] 
