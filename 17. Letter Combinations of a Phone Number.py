e4e69277aab68caf8ad0cc0740405106cea9bb5c
funnydman <crazydzmitry@gmail.com>
2022-07-27 21:28:04 +0200

    Update 17. Letter Combinations of a Phone Number.py

--- 17. Letter Combinations of a Phone Number.py
+++ 17. Letter Combinations of a Phone Number.py
@@ -15,3 +15,31 @@ class Solution:
         }
         string = [mapping[d] for d in digits]
         return map(''.join, product(*string))
+    
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        def backtrack(digits, curr, index):
+            if len(digits) == index:
+                result.append(''.join(curr))
+                return 
+            
+            for i in mapping[digits[index]]:
+                curr.append(i)
+                backtrack(digits, curr, index + 1)
+                curr.pop()
+        if not digits:
+            return []
+        result = []
+        mapping = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        
+        backtrack(digits, [], 0)
+        return result
