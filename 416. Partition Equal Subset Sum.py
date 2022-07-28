5e46576074f1752569dd7f12b534b952ce712e4f
funnydman <crazydzmitry@gmail.com>
2022-07-28 18:33:11 +0200

    Update 416. Partition Equal Subset Sum.py

--- 416. Partition Equal Subset Sum.py
+++ 416. Partition Equal Subset Sum.py
@@ -55,5 +55,25 @@ class Solution:
                 subset.add(elem + num)
             aset |= subset
         return half_sum in aset
-
+    
+# two dimensional DP        
+class Solution:
+    def canPartition(self, nums):
+        total_sum = sum(nums)
+        if total_sum & 1: return False
         
+        half_sum = total_sum // 2
+        
+        dp = [[False]*len(range(half_sum + 1)) for _ in range(len(nums))]
+        
+        for i in range(len(dp)):
+            dp[i][0] = True
+            
+        for i in range(len(nums)):
+            for j in range(half_sum+1):
+                if j < nums[i]:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i]]
+                    
+        return dp[len(nums)-1][half_sum]
