9013b5038318e328656ca3f636e5fc9fe813117b
funnydman <crazydzmitry@gmail.com>
2022-08-04 19:15:17 +0200

    Update 451. Sort Characters By Frequency.py

--- 451. Sort Characters By Frequency.py
+++ 451. Sort Characters By Frequency.py
@@ -27,3 +27,20 @@ class Solution:
             bucket[count].append(key)
         
         return ''.join(i*c[i] for i in list(chain(*bucket))[::-1])
+    
+from queue import PriorityQueue
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        pq = PriorityQueue()
+
+        c = Counter(s)
+
+        for key, count in c.items():
+            pq.put((-count, key))
+
+        res = ''
+
+        while not pq.empty():
+            count, char = pq.get()
+            res += -count * char
+        return res
