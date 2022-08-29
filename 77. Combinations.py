e72ed869e6c6de01106b939de0e0fc20cd433a6d
funnydman <crazydzmitry@gmail.com>
2022-08-29 18:05:18 +0200

    Update 77. Combinations.py

--- 77. Combinations.py
+++ 77. Combinations.py
@@ -1,5 +1,19 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        alist = list(range(1, n+1))
+        result = []
+        stack = [[alist, [], k]]
+        while stack:
+            a, curr, k = stack.pop()
+            if k == 0:
+                result.append(curr)
+            else:
+                for i in range(len(a)):
+                    stack.append([a[i+1:], curr + [a[i]], k - 1])
+        return result
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
         def gen_k_comb(alist, k):
             for i in range(len(alist)):
                 if k == 1:
