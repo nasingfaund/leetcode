3bc08c18c413de981abcbe1ea427aebb68f90a90
funnydman <crazydzmitry@gmail.com>
2022-09-06 16:30:07 +0200

    Update 12. Integer to Roman.py

--- 12. Integer to Roman.py
+++ 12. Integer to Roman.py
@@ -1,3 +1,37 @@
+
+    # a bit improved
+    class Solution:
+    def intToRoman(self, val: int) -> str:
+        divisors = [
+            (1000, 'M'),
+            (900, 'CM'),
+            (500, 'D'),
+            (400, 'CD'),
+            (100, 'C'),
+            (90, 'XC'),
+            (50, 'L'),
+            (40, 'XL'),
+            (10, 'X'),
+            (9, 'IX'),
+            (5, 'V'),
+            (4, 'IV'),
+            (1, 'I')
+        ]
+
+        pointer = 0
+        result = ''
+
+        while val:
+            d, symbol = divisors[pointer]
+            rem = val % d
+            if rem != val:
+                result += symbol
+                val -= d
+            else:
+                pointer += 1
+
+        return result
+
 class Solution:
     def intToRoman(self, val: int) -> str:
         mapping = {
