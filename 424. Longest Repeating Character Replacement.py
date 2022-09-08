5eba0fe82706dbd660074abb94fe011fcf59088a
funnydman <crazydzmitry@gmail.com>
2022-09-08 15:20:35 +0200

    Update 424. Longest Repeating Character Replacement.py

--- 424. Longest Repeating Character Replacement.py
+++ 424. Longest Repeating Character Replacement.py
@@ -1,4 +1,24 @@
 class Solution:
+    def characterReplacement(self, string: str, k: int) -> int:
+        max_len = 0
+        max_repeating = 0
+        left = right = 0
+        adict = defaultdict(int)
+        while right < len(string):
+            adict[string[right]] += 1
+
+            max_repeating = max(max_repeating, adict[string[right]])
+
+            if right - left + 1 - max_repeating > k:
+                adict[string[left]] -= 1
+                if adict[string[left]] == 0:
+                    adict.pop(string[left])
+                left += 1
+            max_len = max(max_len, right - left + 1)
+            right +=1
+        return max_len
+
+class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         max_len = 0
         max_repeating = 0
