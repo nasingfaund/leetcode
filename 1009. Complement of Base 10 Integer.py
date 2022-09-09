30444fe5903e0cc82548b7474f937c080b43f80b
funnydman <crazydzmitry@gmail.com>
2022-09-09 14:08:52 +0200

    Update 1009. Complement of Base 10 Integer.py

--- 1009. Complement of Base 10 Integer.py
+++ 1009. Complement of Base 10 Integer.py
@@ -10,3 +10,9 @@ class Solution:
         return n        
             
         
+class Solution:
+      def bitwiseComplement(self, N: int) -> int:
+            c = 1
+            while c < N: 
+                c = (c << 1) + 1
+            return N ^ c
