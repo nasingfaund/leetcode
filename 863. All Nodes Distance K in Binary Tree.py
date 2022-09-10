a2b85876be66f2bb305b2281d5730803f5222e64
funnydman <crazydzmitry@gmail.com>
2022-09-10 12:09:53 +0200

    Create 863. All Nodes Distance K in Binary Tree.py

---
+++ 863. All Nodes Distance K in Binary Tree.py
@@ -0,0 +1,28 @@
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        def dfs(root, parent=None):
+            if root:
+                root.parent = parent
+                dfs(root.left, root)
+                dfs(root.right, root)
+        
+        dfs(root)
+        result = []
+        
+        queue = deque([(target, 0)])
+        visited = {target}
+        
+        while queue:
+            if queue[0][1] == k:
+                return [node.val for node, d in queue]
+            
+            node, d = queue.popleft()
+            
+            for n in (node.left, node.right, node.parent):
+                if n and n not in visited:
+                    visited.add(n)
+                    queue.append((n, d+1))
+        return []
+            
+        
+        
