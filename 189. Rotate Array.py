21f3cd1fc85322419e2e756467a2998e838f5d29
funnydman <crazydzmitry@gmail.com>
2022-09-10 14:23:48 +0200

    Update 189. Rotate Array.py

--- 189. Rotate Array.py
+++ 189. Rotate Array.py
@@ -1,6 +1,6 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        for _ in range(k):
+        for _ in range(k % len(nums)):
             elem = nums.pop()
             nums.insert(0, elem)
         
