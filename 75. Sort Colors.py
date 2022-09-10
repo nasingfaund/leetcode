2d5d77f606b5556ce287edb32714b7cb79669d22
funnydman <crazydzmitry@gmail.com>
2022-09-10 13:50:27 +0200

    Update 75. Sort Colors.py

--- 75. Sort Colors.py
+++ 75. Sort Colors.py
@@ -1,15 +1,20 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        first = 0
-        second = len(nums) - 1
-        i = 0
-        while i <= second:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        right = len(nums) - 1
+        i = 0 
+        while i <= right:
             if nums[i] == 0:
-                nums[i], nums[first] = nums[first], nums[i]
-                i+=1
-                first += 1
+                nums[i], nums[left] = nums[left], nums[i]
+                left +=1
+                i += 1
             elif nums[i] == 1:
-                i+=1
-            else:
-                nums[i], nums[second] = nums[second], nums[i]
-                second -= 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[right] = nums[right], nums[i]
+                right -= 1
+                
+        return nums
