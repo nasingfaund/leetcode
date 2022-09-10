fc854580a546c468609fdcab11094e4573a0dc76
funnydman <crazydzmitry@gmail.com>
2022-09-10 14:47:38 +0200

    Update 189. Rotate Array.py

--- 189. Rotate Array.py
+++ 189. Rotate Array.py
@@ -4,3 +4,22 @@ class Solution:
             elem = nums.pop()
             nums.insert(0, elem)
         
+        
+# reverse the first n - k elements
+# reverse the rest of them
+# reverse the entire array        
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        end = len(nums) - 1
+        k = k % len(nums)
+        self.reverse(nums, 0, end - k)
+        self.reverse(nums, end - k + 1, end)
+        self.reverse(nums, 0, end)
+        return nums
+        
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+        
