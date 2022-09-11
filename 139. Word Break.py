823e7b85a8a948963c84ec8d7093b5abd5070826
funnydman <crazydzmitry@gmail.com>
2022-09-11 14:25:22 +0200

    Update 139. Word Break.py

--- 139. Word Break.py
+++ 139. Word Break.py
@@ -16,3 +16,19 @@ class Solution:
                 
         return helper('')
         
+# don't explore unneded branches:
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        @cache
+        def helper(curr_string):
+            if curr_string == '':
+                return True
+            for word in wordDict:
+                if not curr_string.startswith(word):
+                    continue
+                if helper(curr_string.removeprefix(word)):
+                    return True
+            return False
+                
+        return helper(s)
+        
