292242cedc22d26e88b51ce307936137b0665339
funnydman <crazydzmitry@gmail.com>
2022-09-11 14:21:06 +0200

    Create 139. Word Break.py

---
+++ 139. Word Break.py
@@ -0,0 +1,18 @@
+# memory limit - crap!
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        def helper(curr_string):
+            if curr_string == s:
+                return True
+            if len(curr_string) > len(s):
+                return False
+            for word in wordDict:
+                if helper(curr_string + word):
+                    return True
+            return False
+            
+                
+                
+        return helper('')
+        
